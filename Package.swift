d4bacc7c7d6a9e5ef937c276f65b35a48edcf0fb
Wooseong Kim <carpe@29cm.co.kr>
2023-11-02 17:16:08 +0900

    Package.swift 추가

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+  name: "TouchAreaInsets",
+  platforms: [.iOS(.v8)],
+  products: [
+    .library(
+      name: "TouchAreaInsets",
+      targets: ["TouchAreaInsets"]
+    ),
+  ],
+  targets: [
+    .target(
+      name: "TouchAreaInsets",
+      path: "Sources"
+    )
+  ],
+  swiftLanguageVersions: [.v5]
+)
